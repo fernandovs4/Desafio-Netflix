2dba10b962bd58d3624e7da8f957e7212dd1aeae
fernandovs4 <fervsantos3000@gmail.com>
2023-04-18 21:28:09 -0300

    fatorando calculaAuto

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ df = pd.read_csv('ratings_small.csv', sep=',')
 df_ratings = df.pivot_table(index='userId', columns='movieId', values='rating')
 #Substitui os valores NaN pela média de cada coluna
 df = df_ratings.fillna(df_ratings.mean())
-df = df_ratings.to_numpy()
+df = df.to_numpy()
 B = df.copy()
-x = 300
+x = 100
 numero_esperado = B[x-1][x-1]
 B[x-1][x-1] = 1
 sigma, U, V_transposto = calculaAuto(B)
